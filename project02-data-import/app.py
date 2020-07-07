6398e4fa5620acdf7d59691293cb3fb4a3e66f65
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-07 08:25:13 +0100

    Finish streamlit app for project02

--- project02-data-import/app.py
+++ project02-data-import/app.py
@@ -11,12 +11,33 @@ def get_movies(page_number):
     return requests.get(url).json()
 
 
+def get_movie(movie_id):
+    query = f"https://api.themoviedb.org/3/movie/{movie_id}?{api_key}"
+    return requests.get(query).json()
+
+
 def main():
     st.title("Project 2 - Data Import")
     data = get_movies(1)
 
-    st.selectbox("Change page", tuple(i + 1 for i in range(data["total_pages"])))
+    page_num = st.sidebar.selectbox(
+        "Change page", tuple(i + 1 for i in range(data["total_pages"]))
+    )
+    data = get_movies(page_num)
 
+    df = pd.DataFrame(data["results"])
+    df.set_index("id", inplace=True)
+    st.dataframe(df)
+
+    movie_name = st.sidebar.selectbox("Change movie", tuple(df["title"].values))
+
+    st.subheader(movie_name)
+    movie_data = get_movie(df[df["title"] == movie_name].index.values.astype(int)[0])
+    normalized_data = pd.json_normalize(movie_data, sep="_").T
+    st.table(normalized_data)
+
+    if st.button("Download movie data"):
+        movie_data.to_json(f"{movie_name}.json", orient="records")
 
 
 main()
