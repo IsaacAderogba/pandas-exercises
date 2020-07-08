7787918def78488b2df43d0477c5641f11d4a5b7
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-08 06:51:14 +0100

    Initial streamlit app for project 03

---
+++ project03-data-cleaning/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+def render_header():
+    st.title("Project 3 - Data Cleaning")
+    st.subheader("Tidying up messy datasets")
+
+
+def main():
+    render_header()
+
+
+main()
