dd9e7b8c463a5d8841cb272c33b7054827b21386
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-09 06:15:17 +0100

    Render header and summary info

--- project03-data-cleaning/app.py
+++ project03-data-cleaning/app.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -8,8 +9,36 @@ def render_header():
     st.subheader("Tidying up messy datasets")
 
 
+def render_summary_info(content: pd.DataFrame):
+    content_info = StringIO()
+    content.info(buf=content_info)
+    str_ = content_info.getvalue()
+
+    lines = str_.split("\n")
+    table = StringIO("\n".join(lines[3:-3]))
+    datatypes = pd.read_table(
+        table, delim_whitespace=True, names=["column", "count", "null", "dtype"]
+    )
+    datatypes.set_index("column", inplace=True)
+
+    info = "\n".join(lines[0:2] + lines[-2:-1])
+    st.subheader("Dataframe info")
+    st.text(info)
+    st.table(datatypes)
+
+    st.subheader("Summary statistics")
+    st.table(content.describe(include="all").T)
+    return info, datatypes
+
+
 def main():
+    # setup
+    df = pd.read_csv("movies_metadata.csv", low_memory=False)
+
+    # render header and overview info
     render_header()
+    if st.sidebar.checkbox("View summary information"):
+        render_summary_info(df)
 
 
 main()
