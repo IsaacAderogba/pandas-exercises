810d7ce06bb40640dd29808154638505e13d5f91
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-07-15 07:02:18 +0100

    Add limit to value counts

--- project03-data-cleaning/app.py
+++ project03-data-cleaning/app.py
@@ -1,7 +1,6 @@
 from io import StringIO
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 
 def render_header():
@@ -30,12 +29,13 @@ def render_summary_info(content: pd.DataFrame):
 
     return info, datatypes
 
+
 def clean_data(df: pd.DataFrame):
     column_names = list(df.columns)
     column_name = st.sidebar.selectbox("Inspect column", column_names)
 
     st.subheader(f"{column_name} value counts")
-    st.table(df[column_name].value_counts())
+    st.table(df[column_name].value_counts().head(10))
 
     columns_to_remove = st.sidebar.multiselect("Drop columns", column_names)
     return df.drop(columns=columns_to_remove)
@@ -53,5 +53,6 @@ def main():
 
     # render cleaned information
     render_summary_info(clean_df)
+
 
 main()
